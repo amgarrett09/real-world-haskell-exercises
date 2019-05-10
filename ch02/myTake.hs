8e8dfc015c4e99e747b9b0cb1da1302295e9d0cc
Alex Garrett <agarrettR8@gmail.com>
2019-05-09 22:42:37 -0400

    refactored myTake to use pattern matching

--- ch02/myTake.hs
+++ ch02/myTake.hs
@@ -1,8 +1,6 @@
 -- For practice, I implemented my own version of take, which returns the first
 -- n elements of a list
-myTake n xs
-    | n <= 0 || null xs = []
-    | n == 1 = (head xs) : []
-    | otherwise = (head xs) : myTake (n - 1) (tail xs)
-
-
+myTake n (x:xs)
+    | n <= 0 = []
+    | n == 1 || null xs = x : []
+    | otherwise = x : myTake (n - 1) xs
